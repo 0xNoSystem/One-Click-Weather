6c50bb5f0fdf4231ea375a5b596bd747b403eaf9
0xNoSystem <joesawayajsx@gmail.com>
2023-10-13 17:04:23 +0200

    Include server Node.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, (req,res)=>{
+    console.log('Listening on port '+ port);
+})
